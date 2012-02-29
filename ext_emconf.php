a3ddbbd6c34b3bb66f19e77f884649be8662e381
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-29 11:06:13 +0000

    [TASK] Small fixes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simulatefe/trunk@58761 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "simulatefe".
 #
-# Auto generated 05-02-2012 18:04
+# Auto generated 27-02-2012 11:16
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:28:{s:21:"ExtensionBuilder.json";s:4:"8a20";s:12:"ext_icon.gif";s:4:"6759";s:17:"ext_localconf.php";s:4:"4181";s:14:"ext_tables.php";s:4:"d9ea";s:14:"ext_tables.sql";s:4:"d41d";s:37:"Classes/Controller/UserController.php";s:4:"1b28";s:29:"Classes/Domain/Model/User.php";s:4:"93a2";s:44:"Classes/Domain/Repository/UserRepository.php";s:4:"7e2e";s:31:"Classes/Utilities/Utilities.php";s:4:"84d1";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"935f";s:45:"Configuration/FlexForms/flexform_simulate.xml";s:4:"94a7";s:26:"Configuration/TCA/User.php";s:4:"dac6";s:38:"Configuration/TypoScript/constants.txt";s:4:"5646";s:34:"Configuration/TypoScript/setup.txt";s:4:"706e";s:40:"Resources/Private/Language/locallang.xml";s:4:"cc7e";s:76:"Resources/Private/Language/locallang_csh_tx_simulatefe_domain_model_user.xml";s:4:"90e5";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"8d70";s:38:"Resources/Private/Layouts/Default.html";s:4:"90c0";s:42:"Resources/Private/Partials/FormErrors.html";s:4:"f5bc";s:47:"Resources/Private/Partials/User/FormFields.html";s:4:"d41d";s:47:"Resources/Private/Partials/User/Properties.html";s:4:"6237";s:42:"Resources/Private/Templates/User/List.html";s:4:"1c3d";s:44:"Resources/Private/Templates/User/Switch.html";s:4:"576b";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:58:"Resources/Public/Icons/tx_simulatefe_domain_model_user.gif";s:4:"905a";s:44:"Tests/Unit/Controller/UserControllerTest.php";s:4:"2a7a";s:36:"Tests/Unit/Domain/Model/UserTest.php";s:4:"6d19";s:14:"doc/manual.sxw";s:4:"c4aa";}',
+	'_md5_values_when_last_written' => 'a:25:{s:21:"ExtensionBuilder.json";s:4:"8a20";s:12:"ext_icon.gif";s:4:"6759";s:17:"ext_localconf.php";s:4:"6b2d";s:14:"ext_tables.php";s:4:"d9ea";s:14:"ext_tables.sql";s:4:"d41d";s:37:"Classes/Controller/UserController.php";s:4:"318b";s:29:"Classes/Domain/Model/User.php";s:4:"93a2";s:44:"Classes/Domain/Repository/UserRepository.php";s:4:"7e2e";s:31:"Classes/Utilities/Utilities.php";s:4:"84d1";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"935f";s:26:"Configuration/TCA/User.php";s:4:"dac6";s:38:"Configuration/TypoScript/constants.txt";s:4:"b264";s:34:"Configuration/TypoScript/setup.txt";s:4:"fc0b";s:40:"Resources/Private/Language/locallang.xml";s:4:"e2fd";s:76:"Resources/Private/Language/locallang_csh_tx_simulatefe_domain_model_user.xml";s:4:"90e5";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"8d70";s:38:"Resources/Private/Layouts/Default.html";s:4:"90c0";s:42:"Resources/Private/Templates/User/List.html";s:4:"b2af";s:44:"Resources/Private/Templates/User/Switch.html";s:4:"576b";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:58:"Resources/Public/Icons/tx_simulatefe_domain_model_user.gif";s:4:"905a";s:43:"Resources/Public/Stylesheets/stylesheet.css";s:4:"8eaf";s:44:"Tests/Unit/Controller/UserControllerTest.php";s:4:"2a7a";s:36:"Tests/Unit/Domain/Model/UserTest.php";s:4:"6d19";s:14:"doc/manual.sxw";s:4:"c4aa";}',
 );
 
 ?>
